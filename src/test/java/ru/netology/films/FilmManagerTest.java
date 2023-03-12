63821e1c8bdb28d0d8adda6627b29648eb28e937
LiudmilaLobanova <liudmilanl@gmail.com>
2023-03-12 00:35:46 -0800

    Initial commit

---
+++ src/test/java/ru/netology/films/FilmManagerTest.java
@@ -0,0 +1,46 @@
+package ru.netology.films;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FilmManagerTest {
+
+    @Test
+    public void shouldAddFilms() {
+        String film1 = "Matrix";
+        String film2 = "Don't look up";
+        String film3 = "Forrest Gump";
+
+        FilmManager manager = new FilmManager();
+        manager.addFilm(film1);
+        manager.addFilm(film2);
+        manager.addFilm(film3);
+
+        String[] expected = {film1, film2, film3};
+        String[] actual = manager.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void shouldFindLastFilmFirst() {
+        String film1 = "Matrix";
+        String film2 = "Don't look up";
+        String film3 = "Forrest Gump";
+
+        FilmManager manager = new FilmManager();
+        manager.addFilm(film1);
+        manager.addFilm(film2);
+        manager.addFilm(film3);
+
+
+        String[] expected = {film3, film2, film1};
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+
+}
